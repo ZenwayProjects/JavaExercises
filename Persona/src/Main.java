8f34c50ef9eec93678361ca08a9f48328fa98a13
David Oliveros <61753685+ZenwayProjects@users.noreply.github.com>
2023-01-01 20:03:29 -0500

    Ejercicio en Java | Keyword Persona |
    Privacidad y encapsulación básica en Java | Java Basic Encapsulation and Privacy |

---
+++ Persona/src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Persona Ingeniero = new Persona();
+
+        Ingeniero.setEdad(21);
+        Ingeniero.setNombre("David");
+        Ingeniero.setTel(8753545);
+
+
+        System.out.println(Ingeniero.getNombre() + "\n" +  Ingeniero.getEdad() + "\n" + Ingeniero.getTel() );
+    }
+}
+
+class Persona{
+    private int edad;
+    private String nombre;
+    private int tel;
+
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public int getEdad(){
+        return this.edad;
+    }
+
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+
+
+
+    public void setTel(int tel){
+        this.tel = tel;
+    }
+    public int getTel(){
+        return this.tel;
+    }
+
+
+}
